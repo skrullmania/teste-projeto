96133a23cfc358fd01e2596f353bc69fd6f687a5
Skrullmania <skrullmania@gmail.com>
2018-10-19 10:01:11 -0400

    exemplo de merda

--- Lui.java
+++ Lui.java
@@ -1,6 +1,6 @@
 public class Lui{
     public static void main(String []args){
         System.out.print("boa lui\n");
-        System.out.println("sou tipo mid")
+        System.out.println("sou tipo mid");
     }
 }
